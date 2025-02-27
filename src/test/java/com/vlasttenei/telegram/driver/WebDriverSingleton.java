c9290fcdcebb1a20f8308a520293282392375ae9
ext_arusanov <ext_arusanov@rencredit.ru>
2025-02-27 10:15:51 +0300

    telegram-game. create WebDriverSingleton

---
+++ src/test/java/com/vlasttenei/telegram/driver/WebDriverSingleton.java
@@ -0,0 +1,27 @@
+package com.vlasttenei.telegram.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebDriverSingleton {
+    private static WebDriver driver;
+
+    private WebDriverSingleton() {} // Закрываем создание объектов
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
