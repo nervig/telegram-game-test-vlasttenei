48421a31c5c0ab000a1042961b2048463c1bf602
ext_arusanov <ext_arusanov@rencredit.ru>
2025-02-27 10:15:02 +0300

    telegram-game. create BaseTest

---
+++ src/test/java/com/vlasttenei/telegram/tests/BaseTest.java
@@ -0,0 +1,24 @@
+package com.vlasttenei.telegram.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+//    @AfterClass
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+}
