7a4b57b6b7ebe9ba0c9a534fd0b327570d5bbce8
ext_arusanov <ext_arusanov@rencredit.ru>
2025-03-02 10:21:45 +0300

    telegram-game. update CheckingCatalogTest

--- src/test/java/com/vlasttenei/telegram/tests/CheckingCatalogTest.java
+++ src/test/java/com/vlasttenei/telegram/tests/CheckingCatalogTest.java
@@ -1,14 +1,23 @@
 package com.vlasttenei.telegram.tests;
 
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class CheckingCatalogTest extends BaseTest {
+    public CheckingCatalogTest() {
+    }
 
-    @Test(priority = 2)
+    @Test(
+            priority = 2
+    )
     public void startGame1() {
         driver.get("https://web.telegram.org/");
-
-        System.out.println("Тест 1: проверка работы игры.");
-        // Дополнительные шаги взаимодействия с ботом
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10L));
+        WebElement logInByPhoneButton = (WebElement)wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@alt='Зов Теней - Тест']")));
+        logInByPhoneButton.click();
     }
 }
