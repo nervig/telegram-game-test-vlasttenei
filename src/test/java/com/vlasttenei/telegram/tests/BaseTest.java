de30990ebf1a27fb8c2e34b52fded5d51159fc1d
ext_arusanov <ext_arusanov@rencredit.ru>
2025-03-02 10:11:14 +0300

    telegram-game. update BaseTest

--- src/test/java/com/vlasttenei/telegram/tests/BaseTest.java
+++ src/test/java/com/vlasttenei/telegram/tests/BaseTest.java
@@ -3,22 +3,25 @@ package com.vlasttenei.telegram.tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
-    protected WebDriver driver;
+    protected static WebDriver driver; // Делаем driver статическим
 
-    @BeforeClass
+    @BeforeSuite
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        if (driver == null) { // Запускаем браузер только если он ещё не запущен
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
     }
 
-//    @AfterClass
-//    public void tearDown() {
-//        if (driver != null) {
-//            driver.quit();
-//        }
-//    }
+    @AfterSuite
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+            driver = null; // Обнуляем driver после завершения всех тестов
+        }
+    }
 }
