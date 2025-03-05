79bc9a7ea3ed96cb7a7b221acb5165a7659c1663
ext_arusanov <ext_arusanov@rencredit.ru>
2025-03-05 08:19:03 +0300

    telegram-game: Добавление метода checkThatGameIsOpen() в BaseTest
    - Реализован новый метод для проверки и запуска игры
    - Использованы WebDriverWait и ExpectedConditions для надежного поиска элементов
    - Добавлена расширенная логика логирования с обработкой исключений
    - Интегрированы xpath-селекторы для поиска бота и кнопки игры

--- src/test/java/com/vlasttenei/telegram/tests/BaseTest.java
+++ src/test/java/com/vlasttenei/telegram/tests/BaseTest.java
@@ -4,10 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeSuite;
 import com.vlasttenei.telegram.driver.WebDriverSingleton;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import java.util.logging.Logger;
 import java.util.logging.Level;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class BaseTest {
     protected static WebDriver driver;
@@ -23,5 +27,26 @@ public class BaseTest {
     public void setUp() {
         LOGGER.info("Инициализация WebDriver...");
         driver = WebDriverSingleton.getDriver();
+    }
+
+    public void checkThatGameIsOpen() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20L));
+        try {         
+            // Ищем бота по названию
+            WebElement chatItem = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//span[text()='Зов Теней - Тест'] | //img[@alt='Зов Теней - Тест']")));
+            chatItem.click();
+            LOGGER.info("Открыт чат с ботом.");
+            
+            // Теперь ищем кнопку игры
+            WebElement gameButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//span[text()='Профиль'] ")));
+            gameButton.click();
+            LOGGER.info("Игра запущена.");
+            
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Ошибка при запуске игры: " + e.getMessage(), e);
+            throw e;
+        }
     }
 }
